500f0ca9dce75d3181f4f77182a148ff8da4adf5
Mickaël <mchrupek@gmail.com>
2016-12-30 13:18:20 +0100

    first commit

---
+++ php-app/index.php
@@ -0,0 +1,21 @@
+<?php
+$dbhost = $_SERVER['RDS_HOSTNAME'];
+$dbport = $_SERVER['RDS_PORT'];
+$dbname = $_SERVER['RDS_DB_NAME'];
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
+$username = $_SERVER['RDS_USERNAME'];
+$password = $_SERVER['RDS_PASSWORD'];
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+$dbh = null;
+?>
+
+<h1>Hello World from Mrevolution !!!</h1>
+<h3>PHP Version <?= phpversion() ?></h3>
+<h3>PHP Version <?= phpinfo() ?></h3>
+<a href="/static.html">Static HTML Page</a>
